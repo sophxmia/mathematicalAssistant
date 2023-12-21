bf0553b64bc8bdace48974e34300a21a38280505
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-22 00:53:42 +0200

    Add first logic to the methods

--- src/main/java/com/github/sophxmia/equation/Equation.java
+++ src/main/java/com/github/sophxmia/equation/Equation.java
@@ -1,4 +1,27 @@
 package com.github.sophxmia.equation;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Equation {
+    private String equation;
+    private List<Double> roots;
+
+    public Equation(String equation) {
+        this.equation = equation;
+        this.roots = new ArrayList<>();
+    }
+
+    public boolean isValid() {
+        return EquationValidator.isValidParentheses(equation) && EquationValidator.isValidExpression(equation);
+    }
+
+    public void solve() {}
+
+    public String getEquation() {
+        return equation;
+    }
+
+    public List<Double> getRoots() {
+        return roots;
+    }
 }
--- src/main/java/com/github/sophxmia/equation/EquationValidator.java
+++ src/main/java/com/github/sophxmia/equation/EquationValidator.java
@@ -1,4 +1,11 @@
 package com.github.sophxmia.equation;
 
 public class EquationValidator {
+
+    public static boolean isValidExpression(String expression) {return true;}
+    public static boolean isValidParentheses(String equation) {return true;}
+
+    private static boolean isMathOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
 }
