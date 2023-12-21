38f71b51c03b3aba868ce73e631a5bd17273bc08
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-22 00:48:47 +0200

    Add the logic for quadratic equation

--- src/main/java/com/github/sophxmia/Main.java
+++ src/main/java/com/github/sophxmia/Main.java
@@ -6,6 +6,7 @@ import com.github.sophxmia.equation.EquationSearch;
 import com.github.sophxmia.equation.EquationValidator;
 
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -16,18 +17,18 @@ public class Main {
                  Оберіть бажану дію зі списку\s
                     1. Введення рівняння\s
                     2. Пошук рівнянь за коренями\s
-                    0. Вихід""");
+                    0. Вихід\s""");
 
 
         int setChoice = scanner.nextInt();
         scanner.nextLine();
 
-        switch (setChoice){
+        switch (setChoice) {
             case 1:
-                handleEquationInput();
+                handleEquationInput(scanner);
                 break;
             case 2:
-                handleEquationSearch();
+                handleEquationSearch(scanner);
                 break;
             case 0:
                 System.out.println("Дякую за використання програми. До побачення!");
@@ -38,11 +39,32 @@ public class Main {
         }
     }
 
-    private static void handleEquationSearch() {
-        System.out.println("Введіть математичне рівняння:");
+    private static void handleEquationSearch(Scanner scanner) {
+        System.out.println("Введіть корінь для пошуку:");
+
+        double searchRoot = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Рівнянь з заданим коренем не знайдено.");
     }
 
-    private static void handleEquationInput() {
-        System.out.println("Введіть корінь для пошуку:");
+    private static void handleEquationInput(Scanner scanner) {
+        System.out.println("Введіть математичне рівняння:");
+        String equationStr = scanner.nextLine();
+
+        if (EquationValidator.isValidParentheses(equationStr) && EquationValidator.isValidExpression(equationStr)){
+            Equation equation = new Equation(equationStr);
+
+            System.out.println("Введене рівняння: " + equationStr);
+            System.out.println("Рівняння коректне: " + equation.isValid());
+
+            if(equation.isValid()){
+                equation.solve();
+                System.out.println("Корені рівняння: ");
+            }
+        }else {
+            System.out.println("Введене рівняння некоректне.");
+        }
+
     }
 }
--- src/main/java/com/github/sophxmia/equation/EquationSolver.java
+++ src/main/java/com/github/sophxmia/equation/EquationSolver.java
@@ -1,4 +1,37 @@
 package com.github.sophxmia.equation;
 
+import com.github.sophxmia.Main;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EquationSolver {
+
+    public static List<Double> solveLinearEquation(double a, double b) {
+        List<Double> roots = new ArrayList<>();
+
+        if (a != 0) {
+            double root = -b / a;
+            roots.add(root);
+        }
+        return roots;
+    }
+
+    public static List<Double> solveQuadraticEquation(double a, double b, double c) {
+        List<Double> roots = new ArrayList<>();
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+            roots.add(root1);
+            roots.add(root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2*a);
+
+            roots.add(root);
+        }
+        return roots;
+    }
 }
